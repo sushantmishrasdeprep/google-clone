1250dc287f214dd4f97cfb5e111f53ef9c66c87f
sushantmishrasdeprep <smsde8800@gmail.com>
2024-02-03 19:32:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# google-clone
+ i have created google clone using reactjs , nextjs , typescript and tailwind css.
---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+let nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: `https`,
+          hostname: `images.unsplash.com`,
+          pathname: `/photo-*`, // Optional: Match specific paths
+        },
+        {
+          protocol: 'https',
+          hostname: 'www.google.com', // Updated hostname
+          pathname: '/images/branding/googlelogo*', // Match specific paths
+        },
+      ],
+    },
+  };
+  
